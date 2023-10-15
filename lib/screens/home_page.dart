8c7b30535cf4d4f2774a3ea90c4a60ffb2e7e68f
Donya <donya.asayesh@outlook.com>
2023-10-16 00:54:48 +0330

    createAppBar

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,32 +1,16 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:flutter_svg/flutter_svg.dart';
-=======
->>>>>>> 476f04509ffee843d83c16595f6d8c01939c0a25
+import 'package:techno_saz/screens/appBar.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return Scaffold(body: Row(
-      
-    ),);
-=======
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(0, 300, 0, 0),
-        child: Center(
-          child: Column(
-            children: [
-              Text(
-                "MyHomePage",
-              ),
-            ],
-          ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: appBar(),
         ),
       ),
     );
->>>>>>> 476f04509ffee843d83c16595f6d8c01939c0a25
   }
 }
