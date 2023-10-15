2748b18f2e8d2db2d584337a77572c5f31c740ab
Omid Haqi <omid.haqi@outlook.com>
2023-10-15 12:59:00 +0330

    addColors

---
+++ lib/res/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  static const Color textColor = Color.fromARGB(255, 54, 47, 47);
+  static const Color secondTextColor = Color.fromRGBO(0, 0, 0, 0.6);
+  static const Color primaryColor = Color.fromARGB(255, 164, 99, 77);
+  static const Color primaryVariantColor = Color.fromARGB(255, 240, 217, 210);
+  static const Color onPrimaryVariantColor = Color.fromARGB(255, 0, 0, 0);
+  static const Color onPrimaryColor = Color.fromARGB(255, 255, 255, 255);
+  static const Color iconColor = Color.fromARGB(255, 113, 40, 16);
+}
+
+class GradientColors {
+  static const List<Color> onBaner = [
+    const Color.fromARGB(255, 50, 26, 18),
+    const Color.fromRGBO(50, 26, 18, 0.12),
+  ];
+}
