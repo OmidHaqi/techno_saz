74dc250db72e8cc92e45452d2fe5fbea8792ead9
Omid Haqi <omid.haqi@outlook.com>
2023-10-16 12:00:43 +0330

    addSplashLogoInPaths

--- lib/res/paths.dart
+++ lib/res/paths.dart
@@ -1,9 +1,10 @@
 import 'package:flutter_svg/svg.dart';
 
 class Adress {
-  static const save ="assets/icons/ic_save.svg";
-  static const bell ="assets/icons/ic_bell.svg";
-  static const feather ="assets/icons/ic_feather.svg";
-  static const menu ="assets/icons/ic_menu.svg";
-  static const search ="assets/icons/ic_search.svg";
+  static const spelashLogo = "assets/images/svg_splash.svg";
+  static const save = "assets/icons/ic_save.svg";
+  static const bell = "assets/icons/ic_bell.svg";
+  static const feather = "assets/icons/ic_feather.svg";
+  static const menu = "assets/icons/ic_menu.svg";
+  static const search = "assets/icons/ic_search.svg";
 }
