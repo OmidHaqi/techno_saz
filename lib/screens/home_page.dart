37d26a1257eca6073a219f1a67212015984bb49f
Omid Haqi <omid.haqi@outlook.com>
2023-10-16 10:48:59 +0330

    sync

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,32 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:flutter_svg/flutter_svg.dart';
-=======
->>>>>>> 476f04509ffee843d83c16595f6d8c01939c0a25
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     return Scaffold(body: Row(
       
     ),);
-=======
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(0, 300, 0, 0),
-        child: Center(
-          child: Column(
-            children: [
-              Text(
-                "MyHomePage",
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
->>>>>>> 476f04509ffee843d83c16595f6d8c01939c0a25
   }
 }
