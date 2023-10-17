f4beffe8ad202e9035d19afe10d0c0647d269860
Donya <donya.asayesh@outlook.com>
2023-10-17 17:11:33 +0330

    creatHashtagModel

--- lib/res/data_class.dart
+++ lib/res/data_class.dart
@@ -1,7 +1,22 @@
+import 'package:techno_saz/models/data_models.dart';
 import 'package:techno_saz/res/paths.dart';
 import 'package:techno_saz/res/strings.dart';
 
 Map homePagePosterMap = {
-  "imageAsset":Adress.banner,
-  "titleOnBanner":Strings.titleOnBanner,
+  "imageAsset": Adress.banner,
+  "titleOnBanner": Strings.titleOnBanner,
 };
+
+List<HashTagModel> tagList = [
+  HashTagModel(title: "جاوا"),
+  HashTagModel(title: "کاتلین"),
+  HashTagModel(title: "وب"),
+  HashTagModel(title: "سلامتی"),
+  HashTagModel(title: "هوش مصنوعی"),
+  HashTagModel(title: "دارت"),
+  HashTagModel(title: "فلاتر"),
+  HashTagModel(title: "تکنولوژی"),
+  HashTagModel(title: "هنر"),
+  HashTagModel(title: "ورزش"),
+  HashTagModel(title: "بیزنس"),
+];
