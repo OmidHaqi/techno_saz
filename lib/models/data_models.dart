64070dcb8e1e0b0d9e97ebfb3ce0eb1ede5fb816
Donya <donya.asayesh@outlook.com>
2023-10-17 17:11:13 +0330

    creatArticlePageModel

--- lib/models/data_models.dart
+++ lib/models/data_models.dart
@@ -1,4 +1,28 @@
-class HashTag {
+class HashTagModel {
   String title;
-  HashTag({required this.title});
+  HashTagModel({required this.title});
+}
+
+class ArticlePageModel {
+  String idArticleUr;
+  String imageArticleUr;
+  String titleArticleUr;
+  String writerProfilePhotoUr;
+  String PublicationDateArticleUr;
+  String PublicationTimeArticleUr;
+  String ContentArticleUr;
+  String likedArticleUr;
+  String commentArticleUr;
+
+  ArticlePageModel({
+    required this.idArticleUr,
+    required this.imageArticleUr,
+    required this.titleArticleUr,
+    required this.writerProfilePhotoUr,
+    required this.PublicationDateArticleUr,
+    required this.PublicationTimeArticleUr,
+    required this.ContentArticleUr,
+    required this.likedArticleUr,
+    required this.commentArticleUr,
+  });
 }
