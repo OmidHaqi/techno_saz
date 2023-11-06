2742020d5bca3e930b558e0491c9dcaaf05950da
Donya <donya.asayesh@outlook.com>
2023-11-06 13:09:58 +0330

    Define address for bookmark page photos

--- lib/res/paths.dart
+++ lib/res/paths.dart
@@ -1,12 +1,12 @@
 class Adress {
-  static const save ="assets/icons/ic_save.svg";
-  static const bell ="assets/icons/ic_bell.svg";
-  static const feather ="assets/icons/ic_feather.svg";
-  static const menu ="assets/icons/ic_menu.svg";
-  static const search ="assets/icons/ic_search.svg";
-  static const banner ="assets/images/img_banner.png";
-  static const wave ="assets/images/vc_wave.svg";
-  static const arrowLeft ="assets/icons/ic_arrow_left.svg";
+  static const save = "assets/icons/ic_save.svg";
+  static const bell = "assets/icons/ic_bell.svg";
+  static const feather = "assets/icons/ic_feather.svg";
+  static const menu = "assets/icons/ic_menu.svg";
+  static const search = "assets/icons/ic_search.svg";
+  static const banner = "assets/images/img_banner.png";
+  static const wave = "assets/images/vc_wave.svg";
+  static const arrowLeft = "assets/icons/ic_arrow_left.svg";
   static const spelashLogo = "assets/images/svg_splash.svg";
   static const recentArticle = "assets/images/img_recent_article.png";
   static const more = "assets/icons/ic_more.svg";
@@ -19,8 +19,30 @@ class Adress {
   static const myArticlesImg = "assets/images/img_my_article.png";
   static const articlesImageidOne = "assets/images/img_rezhim.png";
   static const articlesImageidTwo = "assets/images/img_honey.png";
-  static const articlesImageidThree = "assets/images/img_ConcurrencyControl.png";
-  static const articlesImageidFour = "assets/images/img_emotionalIntelligence.png";
+  static const articlesImageidThree =
+      "assets/images/img_ConcurrencyControl.png";
+  static const articlesImageidFour =
+      "assets/images/img_emotionalIntelligence.png";
+  static const artc_img_Bokmrk_one =
+      "assets/images/bookmarkedPageImages/img_Accounting_Article.png";
+  static const artc_img_Bokmrk_tow =
+      "assets/images/bookmarkedPageImages/img_balletDanceArticle.png";
+  static const artc_img_Bokmrk_three =
+      "assets/images/bookmarkedPageImages/img_BrewingCoffee_Article.png";
+  static const artc_img_Bokmrk_four =
+      "assets/images/bookmarkedPageImages/img_father_doughter_article.png";
+  static const artc_img_Bokmrk_five =
+      "assets/images/bookmarkedPageImages/Img_GroupStudy_Article.png";
+  static const artc_img_Bokmrk_six =
+      "assets/images/bookmarkedPageImages/img_hugArticle.png";
+  static const artc_img_Bokmrk_seven =
+      "assets/images/bookmarkedPageImages/img_mathematicsOnIntelligence_Article.png";
+  static const artc_img_Bokmrk_eight =
+      "assets/images/bookmarkedPageImages/img_ParentingArticle.png";
+  static const artc_img_Bokmrk_nine =
+      "assets/images/bookmarkedPageImages/img_sherakatArticle.png";
+  static const artc_img_Bokmrk_ten =
+      "assets/images/bookmarkedPageImages/img_tangoArticle.png";
   static const writersImageOne = "assets/images/img_writersImageOne.png";
   static const writersImageTwo = "assets/images/img-writersImageTwo.png";
   static const writersImageThree = "assets/images/img_writersImageThree.png";
