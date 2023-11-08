22c2da7ddd4eb3054832bcf74b773e3b7ae95c39
Umut <92772541+OmidHaqi@users.noreply.github.com>
2023-11-08 14:16:43 +0330

    addNavigateForBookMarkedPage

--- lib/screens/components/appBar.dart
+++ lib/screens/components/appBar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:techno_saz/res/colors.dart';
 import 'package:techno_saz/res/paths.dart';
+import 'package:techno_saz/screens/book_Marked_Page.dart';
 import '../../res/strings.dart';
 
 // ignore: must_be_immutable
@@ -10,37 +11,50 @@ class appBar extends StatelessWidget {
   appBar({required this.titleSize});
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Padding(
-          padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
-          child: SvgPicture.asset(Adress.menu),
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
-          child: SvgPicture.asset(Adress.feather),
-        ),
-        Expanded(
-            child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 2, 0, 0),
-          child: Text(
-            Strings.technoSaz_str,
-            style: TextStyle(
-                fontSize: titleSize,
-                color: SolidColors.textColor,
-                fontWeight: FontWeight.bold),
+    return Container(
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
+            child: SvgPicture.asset(Adress.menu),
           ),
-        )),
-        SvgPicture.asset(Adress.search),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(0, 0, 8, 0),
-          child: SvgPicture.asset(Adress.bell),
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(0, 0, 8, 0),
-          child: SvgPicture.asset(Adress.save),
-        ),
-      ],
+          Padding(
+            padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
+            child: SvgPicture.asset(Adress.feather),
+          ),
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 2, 0, 0),
+            child: Text(
+              Strings.technoSaz_str,
+              style: TextStyle(
+                  fontSize: titleSize,
+                  color: SolidColors.textColor,
+                  fontWeight: FontWeight.bold),
+            ),
+          )),
+          SvgPicture.asset(Adress.search),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 8, 0),
+            child: SvgPicture.asset(Adress.bell),
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                
+                context,
+                MaterialPageRoute(
+                  builder: (context) => BookMarkedPage(titleSize: 25),
+                ),
+              );
+            },
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 8, 0),
+              child: SvgPicture.asset(Adress.save),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
