85d8cf1575d4aa2569ee0e007f8d8e86234f657d
Umut <92772541+OmidHaqi@users.noreply.github.com>
2023-11-13 13:57:28 +0330

    optimizedShowDialogs

--- lib/screens/common/widgets.dart
+++ lib/screens/common/widgets.dart
@@ -1,4 +1,3 @@
-
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
@@ -9,73 +8,63 @@ Future customDialoge(
   BuildContext context, {
   final GestureTapCallback? onTap,
   final List<Widget>? actions,
-  // Flash? i,
   var arguments,
 }) {
-  //var flashObserver = i;
   return showDialog(
     context: context,
     builder: (context) {
-      Timer.periodic(Duration(milliseconds: 500), (timer) {
-        
-        
-        if (timer.tick == 500) {
-          timer.cancel();
-        }
-      });
       Timer(
         Duration(seconds: 5),
         () {
           Navigator.pop(context);
-          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage()),);
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => HomePage()),
+          );
         },
       );
-      final high = MediaQuery.of(context).size.width;
+      final size = MediaQuery.of(context).size.width;
       return AlertDialog(
         scrollable: true,
         actions: actions,
         backgroundColor: Colors.white,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(36)),
-        content: SingleChildScrollView(
-          child: Container(
-            alignment: Alignment.topCenter,
-            padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-            width: high / 6 * 3,
-            child: FittedBox(
-              child: Column(
-                children: [
-                  
-                  Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Container(
+        content: Container(
+          alignment: Alignment.topCenter,
+          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+          width: size / 6 * 3,
+          child: FittedBox(
+            child: Column(
+              children: [
+                Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Container(
                         padding: EdgeInsets.all(42),
-                        decoration:BoxDecoration(
-                          color: SolidColors.primaryColor,
-                          shape: BoxShape.circle
-                        )
-                         
-                      ),
-                      Icon(Icons.person, size: 50, color:Colors.white)
-                    ],
-                  ),
-                  SizedBox(height: 15,),
-                  
-                  Text('تبریک!' ,style: TextStyle(fontWeight: FontWeight.bold ,fontSize: 24),),
-                  SizedBox(height: 10),
-                  Text(
-                    '\nحساب شما آماده استفاده است.\n در عرض چند ثانیه به صفحه اصلی \nهدایت خواهید شد',
-                    textAlign: TextAlign.center,
-                    
-                  ),
-                  SizedBox(height: 15),
-                  SpinKitCircle(
-              size: 40,
-              color:SolidColors.primaryColor,
-
-            )
-                ],
-              ),
+                        decoration: BoxDecoration(
+                            color: SolidColors.primaryColor,
+                            shape: BoxShape.circle)),
+                    Icon(Icons.person, size: 50, color: Colors.white)
+                  ],
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  'تبریک!',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  '\nحساب شما آماده استفاده است.\n در عرض چند ثانیه به صفحه اصلی \nهدایت خواهید شد',
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 15),
+                SpinKitCircle(
+                  size: 40,
+                  color: SolidColors.primaryColor,
+                )
+              ],
             ),
           ),
         ),
