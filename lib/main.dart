e03391dbde4d7fedf8ada1ff458d0cf1db13fd31
Umut <92772541+OmidHaqi@users.noreply.github.com>
2023-11-14 13:07:53 +0330

    changeMyAppClassDirInMain.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:techno_saz/res/colors.dart';
 import 'package:techno_saz/screens/splash_screen.dart';
 
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('fa'), // Farsi
+      ],
+      theme: ThemeData(fontFamily: 'IranYekan'),
+      
+      home: MySplash(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
 void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     statusBarColor: SolidColors.primaryVariantColor,
@@ -10,5 +31,5 @@ void main() {
     systemNavigationBarColor: SolidColors.primaryVariantColor,
     systemNavigationBarIconBrightness: Brightness.dark,
   ));
-  runApp(SplashScreen());
+  runApp(MyApp());
 }
