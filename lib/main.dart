6d6573de4625fb29f31ef0fdc37e6b22ea347fea
Umut <92772541+OmidHaqi@users.noreply.github.com>
2023-11-14 13:24:48 +0330

    initThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -8,19 +9,28 @@ import 'package:techno_saz/screens/splash_screen.dart';
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: [
-        Locale('fa'), // Farsi
-      ],
-      theme: ThemeData(fontFamily: 'IranYekan'),
-      
-      home: MySplash(),
-      debugShowCheckedModeBanner: false,
+    return AdaptiveTheme(
+      light: ThemeData(
+        
+        ),
+      dark: ThemeData(
+        
+        ),
+      initial: AdaptiveThemeMode.light,
+      builder:(theme,darkTheme)=> MaterialApp(
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: [
+          Locale('fa'), // Farsi
+        ],
+        theme: ThemeData(fontFamily: 'IranYekan'),
+        
+        home: MySplash(),
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
