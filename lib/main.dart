b9c537c1a7bfe9cc9471c16990479144c7af9254
Umut <92772541+OmidHaqi@users.noreply.github.com>
2023-11-15 05:02:22 +0330

    addSysChInMain.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,63 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:techno_saz/res/colors.dart';
 import 'package:techno_saz/screens/splash_screen.dart';
 
-
 class MyApp extends StatelessWidget {
+  /**
+   * 
+   */
+  final AdaptiveThemeMode? savedThemeMode;
+  const MyApp({required this.savedThemeMode});
+
   @override
   Widget build(BuildContext context) {
+    /**
+     * 
+     */
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: SolidColors.primaryVariantColor,
+        statusBarIconBrightness: Brightness.dark,
+        systemNavigationBarColor: SolidColors.primaryVariantColor,
+        systemNavigationBarIconBrightness: Brightness.dark,
+      ),
+    );
     return AdaptiveTheme(
       light: ThemeData(
-        
-        ),
+        scaffoldBackgroundColor: SolidColors.white,
+        fontFamily: 'IranYekan',
+        colorScheme: ColorScheme(
+            brightness: Brightness.light,
+            primary: const Color.fromARGB(255, 255, 255, 255),
+            onPrimary: const Color.fromARGB(255, 0, 0, 0),
+            secondary: SolidColors.primaryVariantColor,
+            onSecondary: SolidColors.onPrimaryVariantColor,
+            error: Colors.red,
+            onError: SolidColors.white,
+            background: SolidColors.white,
+            onBackground: SolidColors.textColor,
+            surface: SolidColors.white,
+            onSurface: SolidColors.black),
+      ),
       dark: ThemeData(
+fontFamily: 'IranYekan',
+scaffoldBackgroundColor: SolidColors.black,
+            colorScheme: ColorScheme(
+            brightness: Brightness.dark,
+            primary: const Color.fromARGB(255, 26, 21, 19),
+            onPrimary: SolidColors.onPrimaryColor,
+            secondary: const Color.fromARGB(255, 49, 23, 15),
+            onSecondary: SolidColors.onPrimaryVariantColor,
+            error: Colors.red,
+            onError: SolidColors.white,
+            background: const Color.fromARGB(255, 39, 39, 39),
+            onBackground: const Color.fromARGB(255, 255, 255, 255),
+            surface: const Color.fromARGB(255, 0, 0, 0),
+            onSurface: const Color.fromARGB(255, 255, 255, 255)),
         
-        ),
-      initial: AdaptiveThemeMode.light,
-      builder:(theme,darkTheme)=> MaterialApp(
+      ),
+      initial: savedThemeMode ?? AdaptiveThemeMode.light,
+      builder: (theme, darkTheme) => MaterialApp(
+        
         localizationsDelegates: [
           GlobalMaterialLocalizations.delegate,
           GlobalWidgetsLocalizations.delegate,
@@ -26,20 +70,22 @@ class MyApp extends StatelessWidget {
         supportedLocales: [
           Locale('fa'), // Farsi
         ],
-        theme: ThemeData(fontFamily: 'IranYekan'),
-        
+        /**
+         * 
+         */
+        theme: theme,
+        darkTheme: darkTheme,
         home: MySplash(),
         debugShowCheckedModeBanner: false,
       ),
     );
   }
 }
-void main() {
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    statusBarColor: SolidColors.primaryVariantColor,
-    statusBarIconBrightness: Brightness.dark,
-    systemNavigationBarColor: SolidColors.primaryVariantColor,
-    systemNavigationBarIconBrightness: Brightness.dark,
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final savedThemeMode = await AdaptiveTheme.getThemeMode();
+  runApp(MyApp(
+    savedThemeMode: savedThemeMode,
   ));
-  runApp(MyApp());
 }
