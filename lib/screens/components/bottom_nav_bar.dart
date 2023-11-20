78a64cb7b251ad4dc1ac0339255845654e4da623
Umut <92772541+OmidHaqi@users.noreply.github.com>
2023-11-20 18:01:04 +0330

    fixSomeBugs

--- lib/screens/components/bottom_nav_bar.dart
+++ lib/screens/components/bottom_nav_bar.dart
@@ -1,11 +1,9 @@
-import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:techno_saz/res/colors.dart';
 import 'package:techno_saz/res/paths.dart';
 import 'package:techno_saz/res/strings.dart';
 import 'package:techno_saz/screens/articles_management_page.dart';
-import 'package:techno_saz/screens/home_page.dart';
 import 'package:techno_saz/screens/logIn_two_page.dart';
 
 class BottomNavBar extends StatefulWidget {
