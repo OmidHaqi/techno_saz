c859081449108a99edd3bd1c3dcc6859c441487b
Donya <donya.asayesh@outlook.com>
2023-11-20 17:07:52 +0330

    createDrawerMenuHandle

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -4,7 +4,9 @@ import 'package:flutter_svg/svg.dart';
 import 'package:techno_saz/res/colors.dart';
 import 'package:techno_saz/res/paths.dart';
 import 'package:techno_saz/res/strings.dart';
+import 'package:techno_saz/screens/book_Marked_Page.dart';
 import 'package:techno_saz/screens/components/appBar.dart';
+import 'package:techno_saz/screens/notifications.dart';
 
 import 'components/bottom_nav_bar.dart';
 import 'components/hashtag.dart';
@@ -12,7 +14,14 @@ import 'components/home_pages_banner.dart';
 import 'components/my_articles.dart';
 import 'components/recent_article.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+final GlobalKey<ScaffoldState> _key = GlobalKey();
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -20,6 +29,7 @@ class HomePage extends StatelessWidget {
     double titleSize = 20;
     return SafeArea(
       child: Scaffold(
+        key: _key,
         drawer: Drawer(
           width: 200,
           child: ListView(
@@ -55,7 +65,7 @@ class HomePage extends StatelessWidget {
                         height: 10,
                       ),
                       ListTile(
-                        onTap: (){},
+                        onTap: () {},
                         leading: SvgPicture.asset(
                           Address.ic_profile_drewer,
                           colorFilter: ColorFilter.mode(
@@ -68,7 +78,7 @@ class HomePage extends StatelessWidget {
                         title: Text("پروفایل"),
                       ),
                       ListTile(
-                        onTap: (){},
+                        onTap: () {},
                         leading: SvgPicture.asset(
                           Address.ic_share_drewer,
                           colorFilter: ColorFilter.mode(
@@ -81,7 +91,7 @@ class HomePage extends StatelessWidget {
                         title: Text("اشتراک گذاری"),
                       ),
                       ListTile(
-                        onTap: (){},
+                        onTap: () {},
                         leading: SvgPicture.asset(
                           Address.ic_update_drewer,
                           colorFilter: ColorFilter.mode(
@@ -94,7 +104,7 @@ class HomePage extends StatelessWidget {
                         title: Text("بروزرسانی"),
                       ),
                       ListTile(
-                        onTap: (){},
+                        onTap: () {},
                         leading: SvgPicture.asset(
                           Address.ic_github_drewer,
                           colorFilter: ColorFilter.mode(
@@ -107,7 +117,7 @@ class HomePage extends StatelessWidget {
                         title: Text("گیت هاب"),
                       ),
                       ListTile(
-                        onTap: (){},
+                        onTap: () {},
                         leading: SvgPicture.asset(
                           Address.ic_about_drewer,
                           colorFilter: ColorFilter.mode(
@@ -127,12 +137,113 @@ class HomePage extends StatelessWidget {
           ),
         ),
         appBar: AppBar(
-          automaticallyImplyLeading: true,
+          automaticallyImplyLeading: false,
           elevation: 0,
           title: Padding(
-            padding: const EdgeInsets.fromLTRB(12, 15, 12, 15),
-            child: HomePagesAppBar(titleSize: titleSize),
-          ),
+              padding: const EdgeInsets.fromLTRB(12, 15, 12, 15),
+              child: Container(
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
+                      child: InkWell(
+                        onTap: () {
+                          _key.currentState!.openDrawer();
+                        },
+                        child: SvgPicture.asset(
+                          Address.menu,
+                          colorFilter: ColorFilter.mode(
+                              AdaptiveTheme.of(context).brightness ==
+                                      Brightness.dark
+                                  ? SolidColors.primaryVariantColor
+                                  : SolidColors.iconColor,
+                              BlendMode.srcIn),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
+                      child: SvgPicture.asset(Address.feather),
+                    ),
+                    Expanded(
+                        child: Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 2, 0, 0),
+                      child: Text(
+                        Strings.technoSaz_str,
+                        style: TextStyle(
+                            fontSize: titleSize, fontWeight: FontWeight.bold),
+                      ),
+                    )),
+                    Switch(
+                        inactiveThumbImage: AssetImage(Address.changeThemeDark),
+                        activeThumbImage: AssetImage(Address.changeThemeLight),
+                        value: AdaptiveTheme.of(context).mode.isDark,
+                        onChanged: (value) {
+                          if (value) {
+                            AdaptiveTheme.of(context).setDark();
+                          } else {
+                            AdaptiveTheme.of(context).setLight();
+                          }
+                        }),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 8),
+                      child: SvgPicture.asset(
+                        Address.search,
+                        colorFilter: ColorFilter.mode(
+                            AdaptiveTheme.of(context).brightness ==
+                                    Brightness.dark
+                                ? SolidColors.primaryVariantColor
+                                : SolidColors.iconColor,
+                            BlendMode.srcIn),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 8, 0),
+                      child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    Notifications(titleSize: 25),
+                              ),
+                            );
+                          },
+                          child: SvgPicture.asset(
+                            Address.bell,
+                            colorFilter: ColorFilter.mode(
+                                AdaptiveTheme.of(context).brightness ==
+                                        Brightness.dark
+                                    ? SolidColors.primaryVariantColor
+                                    : SolidColors.iconColor,
+                                BlendMode.srcIn),
+                          )),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => BookMarkedPage(titleSize: 25),
+                          ),
+                        );
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 8, 0),
+                        child: SvgPicture.asset(
+                          Address.save,
+                          colorFilter: ColorFilter.mode(
+                              AdaptiveTheme.of(context).brightness ==
+                                      Brightness.dark
+                                  ? SolidColors.primaryVariantColor
+                                  : SolidColors.iconColor,
+                              BlendMode.srcIn),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )),
         ),
         body: SingleChildScrollView(
           physics:
