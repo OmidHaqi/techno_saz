f80dd5692c6d74198ebfc426855e29c61ff4ad0b
Donya <donya.asayesh@outlook.com>
2023-11-21 16:13:52 +0330

    30

--- lib/screens/components/recent_article.dart
+++ lib/screens/components/recent_article.dart
@@ -19,7 +19,10 @@ class RecentArticle extends StatelessWidget {
     return Column(
       children: [
         Padding(
-          padding: const EdgeInsets.fromLTRB(15, 15, 20, 15),
+          padding:  EdgeInsets.fromLTRB(size.width / 30.71,
+                   0,
+                    size.width / 30.71,
+                    size.height / 66.57),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
@@ -38,132 +41,139 @@ class RecentArticle extends StatelessWidget {
             ],
           ),
         ),
-        Container(
-          height: size.height / 3,
-          width: size.width / 1.02,
-          child: ListView.builder(
-            physics: BouncingScrollPhysics(),
-            itemCount: DataClass.ArticlePageModelList.length,
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (context, index) {
-              return InkWell(
-                onTap: () {},
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(9, 0, 0, 0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        boxShadow: <BoxShadow>[
-                          BoxShadow(
-                              blurRadius: 1,
-                              color: AdaptiveTheme.of(context).brightness ==
-                                      Brightness.light
-                                  ? Color.fromARGB(43, 164, 99, 77)
-                                  : Color.fromARGB(12, 255, 255, 255),
-                              offset: Offset(1, 2)),
-                        ],
-                        border: Border.all(width: 0.01),
-                        borderRadius: BorderRadius.all(Radius.circular(20))),
-                    // color: Colors.amber,
-                    width: size.width / 2.5,
-                    height: size.height / 3.9,
-                    child: Column(
-                      children: [
-                        Container(
-                          width: size.width / 2.5,
-                          height: size.height / 5.3,
-                          decoration: BoxDecoration(
-                            boxShadow: <BoxShadow>[
-                              BoxShadow(
-                                  blurRadius: 4,
-                                  color: Color.fromARGB(78, 164, 99, 77),
-                                  offset: Offset(2, 4)),
-                            ],
-                            borderRadius: BorderRadius.circular(20),
-                            image: DecorationImage(
-                                image: AssetImage(DataClass
-                                    .ArticlePageModelList[index]
-                                    .imageArticleUr),
-                                fit: BoxFit.cover),
+        Padding(
+          padding:  EdgeInsets.fromLTRB(size.width / 30.71,
+                    0,
+                    size.width / 30.71,
+                    size.height / 66.57),
+          child: Container(
+            // color: Colors.amber,
+            height: size.height / 3,
+            width: size.width / 1.02,
+            child: ListView.builder(
+              physics: BouncingScrollPhysics(),
+              itemCount: DataClass.ArticlePageModelList.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {},
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(9, 0, 0, 0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          boxShadow: <BoxShadow>[
+                            BoxShadow(
+                                blurRadius: 1,
+                                color: AdaptiveTheme.of(context).brightness ==
+                                        Brightness.light
+                                    ? Color.fromARGB(43, 164, 99, 77)
+                                    : Color.fromARGB(12, 255, 255, 255),
+                                offset: Offset(1, 2)),
+                          ],
+                          border: Border.all(width: 0.01),
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                      // color: Colors.amber,
+                      width: size.width / 2.5,
+                      height: size.height / 3.9,
+                      child: Column(
+                        children: [
+                          Container(
+                            width: size.width / 2.5,
+                            height: size.height / 5.3,
+                            decoration: BoxDecoration(
+                              boxShadow: <BoxShadow>[
+                                BoxShadow(
+                                    blurRadius: 4,
+                                    color: Color.fromARGB(78, 164, 99, 77),
+                                    offset: Offset(2, 4)),
+                              ],
+                              borderRadius: BorderRadius.circular(20),
+                              image: DecorationImage(
+                                  image: AssetImage(DataClass
+                                      .ArticlePageModelList[index]
+                                      .imageArticleUr),
+                                  fit: BoxFit.cover),
+                            ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(0, 12, 6, 12),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Container(
-                                // color: Colors.blue,
-                                width: size.width / 3.3,
-                                height: size.height / 20,
-                                child: Text(
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 12, 6, 12),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Container(
+                                  // color: Colors.blue,
+                                  width: size.width / 3.3,
+                                  height: size.height / 20,
+                                  child: Text(
+                                    DataClass.ArticlePageModelList[index]
+                                        .titleArticleUr,
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 2,
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                                SvgPicture.asset(
+                                  Address.more,
+                                  colorFilter: ColorFilter.mode(
+                                      AdaptiveTheme.of(context).brightness ==
+                                              Brightness.dark
+                                          ? SolidColors.primaryVariantColor
+                                          : SolidColors.iconColor,
+                                      BlendMode.srcIn),
+                                )
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(6, 0, 0, 0),
+                            child: Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.fromLTRB(4, 0, 10, 0),
+                                  child: Container(
+                                    height: size.height / 37.28,
+                                    width: size.width / 17.2,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(400),
+                                      image: DecorationImage(
+                                          image: AssetImage(DataClass
+                                              .ArticlePageModelList[index]
+                                              .writerProfilePhotoUr),
+                                          fit: BoxFit.cover),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: size.width / 7,
+                                  child: Text(
+                                    DataClass
+                                        .ArticlePageModelList[index].writersName,
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 1,
+                                    style: TextStyle(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Text(
                                   DataClass.ArticlePageModelList[index]
-                                      .titleArticleUr,
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 2,
+                                      .PublicationDateArticleUr,
                                   style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                  ),
+                                      fontSize: 12, fontWeight: FontWeight.bold),
                                 ),
-                              ),
-                              SvgPicture.asset(
-                                Address.more,
-                                colorFilter: ColorFilter.mode(
-                                    AdaptiveTheme.of(context).brightness ==
-                                            Brightness.dark
-                                        ? SolidColors.primaryVariantColor
-                                        : SolidColors.iconColor,
-                                    BlendMode.srcIn),
-                              )
-                            ],
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(6, 0, 0, 0),
-                          child: Row(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.fromLTRB(4, 0, 10, 0),
-                                child: Container(
-                                  height: size.height / 37.28,
-                                  width: size.width / 17.2,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(400),
-                                    image: DecorationImage(
-                                        image: AssetImage(DataClass
-                                            .ArticlePageModelList[index]
-                                            .writerProfilePhotoUr),
-                                        fit: BoxFit.cover),
-                                  ),
-                                ),
-                              ),
-                              SizedBox(
-                                width: size.width / 7,
-                                child: Text(
-                                  DataClass
-                                      .ArticlePageModelList[index].writersName,
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 1,
-                                  style: TextStyle(
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                              Text(
-                                DataClass.ArticlePageModelList[index]
-                                    .PublicationDateArticleUr,
-                                style: TextStyle(
-                                    fontSize: 12, fontWeight: FontWeight.bold),
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              );
-            },
+                );
+              },
+            ),
           ),
         ),
       ],
