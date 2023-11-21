2ee8a499a3072e33fd80c292935001d86b471187
Donya <donya.asayesh@outlook.com>
2023-11-21 14:38:50 +0330

    11

--- lib/screens/components/recent_article.dart
+++ lib/screens/components/recent_article.dart
@@ -81,7 +81,7 @@ class RecentArticle extends StatelessWidget {
                             borderRadius: BorderRadius.circular(20),
                             image: DecorationImage(
                                 image: AssetImage(DataClass
-                                    .BookMarkedArticlePageModelList[index]
+                                    .ArticlePageModelList[index]
                                     .imageArticleUr),
                                 fit: BoxFit.cover),
                           ),
@@ -96,8 +96,7 @@ class RecentArticle extends StatelessWidget {
                                 width: size.width / 3.3,
                                 height: size.height / 20,
                                 child: Text(
-                                  DataClass
-                                      .BookMarkedArticlePageModelList[index]
+                                  DataClass.ArticlePageModelList[index]
                                       .titleArticleUr,
                                   overflow: TextOverflow.ellipsis,
                                   maxLines: 2,
@@ -132,8 +131,7 @@ class RecentArticle extends StatelessWidget {
                                     borderRadius: BorderRadius.circular(400),
                                     image: DecorationImage(
                                         image: AssetImage(DataClass
-                                            .BookMarkedArticlePageModelList[
-                                                index]
+                                            .ArticlePageModelList[index]
                                             .writerProfilePhotoUr),
                                         fit: BoxFit.cover),
                                   ),
@@ -143,8 +141,7 @@ class RecentArticle extends StatelessWidget {
                                 width: size.width / 7,
                                 child: Text(
                                   DataClass
-                                      .BookMarkedArticlePageModelList[index]
-                                      .writersName,
+                                      .ArticlePageModelList[index].writersName,
                                   overflow: TextOverflow.ellipsis,
                                   maxLines: 1,
                                   style: TextStyle(
@@ -153,7 +150,7 @@ class RecentArticle extends StatelessWidget {
                                 ),
                               ),
                               Text(
-                                DataClass.BookMarkedArticlePageModelList[index]
+                                DataClass.ArticlePageModelList[index]
                                     .PublicationDateArticleUr,
                                 style: TextStyle(
                                     fontSize: 12, fontWeight: FontWeight.bold),
@@ -166,106 +163,6 @@ class RecentArticle extends StatelessWidget {
                   ),
                 ),
               );
-              // return Padding(
-              //   padding: const EdgeInsets.fromLTRB(8, 2, 0, 0),
-              //   child: Container(
-              //     width: size.width / 2,
-              //     height: size.height / 3,
-              //     child: Column(
-              //       children: [
-              //         Padding(
-              //           padding: const EdgeInsets.fromLTRB(12, 0, 6, 0),
-              //           child: Container(
-              //             width: size.width / 2.22,
-              //             height: size.height / 4.75,
-              //             decoration: BoxDecoration(
-              //               boxShadow: <BoxShadow>[
-              //                 BoxShadow(
-              //                     blurRadius: 4,
-              //                     color: Color.fromARGB(78, 164, 99, 77),
-              //                     offset: Offset(2, 4)),
-              //               ],
-              //               borderRadius: BorderRadius.circular(35),
-              //               image: DecorationImage(
-              //                   image: AssetImage(DataClass
-              //                       .ArticlePageModelList[index]
-              //                       .imageArticleUr),
-              //                   fit: BoxFit.cover),
-              //             ),
-              //           ),
-              //         ),
-              //         Padding(
-              //           padding: const EdgeInsets.fromLTRB(12, 12, 12, 12),
-              //           child: Row(
-              //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //             children: [
-              //               SizedBox(
-              //                 width: size.width / 2.8,
-              //                 child: Text(
-              //                   DataClass
-              //                       .ArticlePageModelList[index].titleArticleUr,
-              //                   overflow: TextOverflow.ellipsis,
-              //                   maxLines: 2,
-              //                   style: TextStyle(
-              //                     fontSize: 18,
-              //                     fontWeight: FontWeight.bold,
-              //                   ),
-              //                 ),
-              //               ),
-              //               SvgPicture.asset(
-              //                 Address.more,
-              //                 colorFilter: ColorFilter.mode(
-              //                     AdaptiveTheme.of(context).brightness ==
-              //                             Brightness.dark
-              //                         ? SolidColors.primaryVariantColor
-              //                         : SolidColors.iconColor,
-              //                     BlendMode.srcIn),
-              //               )
-              //             ],
-              //           ),
-              //         ),
-              //         Padding(
-              //           padding: const EdgeInsets.fromLTRB(8, 0, 1, 0),
-              //           child: Row(
-              //             children: [
-              //               Padding(
-              //                 padding: const EdgeInsets.fromLTRB(8, 0, 10, 0),
-              //                 child: Container(
-              //                   height: size.height / 37.28,
-              //                   width: size.width / 17.2,
-              //                   decoration: BoxDecoration(
-              //                     borderRadius: BorderRadius.circular(400),
-              //                     image: DecorationImage(
-              //                         image: AssetImage(DataClass
-              //                             .ArticlePageModelList[index]
-              //                             .writerProfilePhotoUr),
-              //                         fit: BoxFit.cover),
-              //                   ),
-              //                 ),
-              //               ),
-              //               SizedBox(
-              //                 width: size.width / 5.60,
-              //                 child: Text(
-              //                   DataClass
-              //                       .ArticlePageModelList[index].writersName,
-              //                   overflow: TextOverflow.ellipsis,
-              //                   maxLines: 1,
-              //                   style: TextStyle(
-              //                       fontSize: 20, fontWeight: FontWeight.bold),
-              //                 ),
-              //               ),
-              //               SizedBox(
-              //                 width: 5,
-              //               ),
-              //               Text(DataClass.ArticlePageModelList[index]
-              //                   .PublicationDateArticleUr),
-              //             ],
-              //           ),
-              //         )
-              //       ],
-              //     ),
-              //   ),
-              // );
             },
           ),
         ),
