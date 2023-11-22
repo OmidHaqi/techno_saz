690b4784f5479865df2d49010762286ddbc54498
Donya <donya.asayesh@outlook.com>
2023-11-22 15:45:21 +0330

    finish dialog

--- lib/screens/common/dialoge.dart
+++ lib/screens/common/dialoge.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
 
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:techno_saz/res/colors.dart';
 import 'package:techno_saz/screens/home_page.dart';
 
 customeDialogee(
@@ -12,7 +15,7 @@ customeDialogee(
       context: context,
       builder: (context) {
         Timer(
-          Duration(seconds: 10),
+          Duration(seconds:3),
           () {
             Navigator.pop(context);
             Navigator.pushReplacement(
@@ -25,7 +28,60 @@ customeDialogee(
         return AlertDialog(
           scrollable: true,
           actions: actions,
-          
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(36),
+          ),
+          content: Container(
+            padding: EdgeInsets.symmetric(horizontal: 14, vertical: 16),
+            alignment: Alignment.topCenter,
+            width: sizeW / 18,
+            child: FittedBox(
+              child: Column(
+                children: [
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(42),
+                        decoration: BoxDecoration(
+                            color: AdaptiveTheme.of(context).brightness ==
+                                    Brightness.light
+                                ? SolidColors.primaryColor
+                                : SolidColors.primaryVariantColor,
+                            shape: BoxShape.circle),
+                      ),
+                      Icon(Icons.person,
+                          size: 50,
+                          color: AdaptiveTheme.of(context).brightness ==
+                                Brightness.light
+                            ? const Color.fromARGB(255, 255, 255, 255)
+                            : SolidColors.primaryColor)
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    'تبریک!',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    '\nحساب شما آماده استفاده است.\n در عرض چند ثانیه به صفحه اصلی \nهدایت خواهید شد',
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 15),
+                  SpinKitCircle(
+                    size: 40,
+                    color:
+                        AdaptiveTheme.of(context).brightness == Brightness.light
+                            ? SolidColors.primaryColor
+                            : SolidColors.primaryVariantColor,
+                  )
+                ],
+              ),
+            ),
+          ),
         );
       });
 }
