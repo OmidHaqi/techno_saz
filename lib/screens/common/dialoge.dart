716377f55747bbb1134993e7c0433719ccb94954
Donya <donya.asayesh@outlook.com>
2023-11-22 14:06:22 +0330

    1

---
+++ lib/screens/common/dialoge.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:techno_saz/screens/home_page.dart';
+
+customeDialoge(
+  BuildContext context, {
+  final GestureTapCallback? onTap,
+  final List<Widget>? actions,
+}) {
+  return showDialog(
+      context: context,
+      builder: (context) {
+        Timer(
+          Duration,
+          (seconds: 5),
+          () {
+            Navigator.pop(context);
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
+          },
+        );
+        final sizeW = MediaQuery.of(context).size.width;
+        return AlertDialog(scrollable: ,);
+      });
+}
