606b58d04cc881f1c14130d1dfa4a09f98a19f17
Umut <92772541+OmidHaqi@users.noreply.github.com>
2023-11-26 12:57:50 +0330

    refactorToGetX

---
+++ lib/view/splash_screen.dart
@@ -0,0 +1,66 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:techno_saz/res/colors.dart';
+import 'package:techno_saz/res/strings.dart';
+import 'package:techno_saz/view/book_Marked_Page.dart';
+import 'package:techno_saz/view/home_page.dart';
+import 'package:techno_saz/view/login_page.dart';
+
+
+class MySplash extends StatefulWidget {
+  @override
+  _MySplashState createState() => _MySplashState();
+}
+
+class _MySplashState extends State<MySplash> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 1),
+      () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => LogInPage(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                SvgPicture.asset(
+                  "assets/images/svg_splash.svg",
+                ),
+                Text(
+                  Strings.technoSaz_str,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 48,
+                      ),
+                ),
+              ],
+            ),
+            SpinKitCircle(
+              size: 45,
+              color: SolidColors.primaryColor,
+            ),
+            Text(
+              Strings.madeWith_str,
+              
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
