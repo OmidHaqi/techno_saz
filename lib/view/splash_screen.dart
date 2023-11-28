c5fd3ec7a8ea4a8641c7eb1c09a8a4e3f114a94e
Donya <donya.asayesh@outlook.com>
2023-11-28 17:17:03 +0330

    edit

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -4,8 +4,8 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:techno_saz/res/colors.dart';
 import 'package:techno_saz/res/strings.dart';
+import 'package:techno_saz/view/login_page.dart';
 
-import 'package:techno_saz/view/profile.dart';
 
 
 class MySplash extends StatefulWidget {
