881eadc6290b16e5837d9c8423897560eaf02775
Donya <donya.asayesh@outlook.com>
2023-11-29 10:41:13 +0330

    The second profile page

--- lib/view/components/bottom_nav_bar.dart
+++ lib/view/components/bottom_nav_bar.dart
@@ -1,7 +1,7 @@
-import 'package:adaptive_theme/adaptive_theme.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:techno_saz/res/colors.dart';
+
 import 'package:techno_saz/res/common/widgets.dart';
 import 'package:techno_saz/res/paths.dart';
 import 'package:techno_saz/res/strings.dart';
