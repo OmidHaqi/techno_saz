4bc873f5d8a9fee0bc9922f42c02c89c845cbee7
Donya <donya.asayesh@outlook.com>
2023-11-29 10:41:28 +0330

    The second profile page

--- lib/view/second_profile_page.dart
+++ lib/view/second_profile_page.dart
@@ -1,9 +1,9 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:get/get.dart';
+
 import 'package:techno_saz/res/colors.dart';
-import 'package:techno_saz/res/data_class.dart';
+
 import 'package:techno_saz/res/paths.dart';
 import 'package:techno_saz/res/strings.dart';
 import 'package:techno_saz/view/home_page.dart';
