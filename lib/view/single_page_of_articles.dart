be2bcfafe884d01116e60ffcc22ef9b29a356b4c
Donya <donya.asayesh@outlook.com>
2023-11-29 13:37:31 +0330

    Creating a single page of articles

---
+++ lib/view/single_page_of_articles.dart
@@ -0,0 +1,81 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:techno_saz/res/colors.dart';
+import 'package:techno_saz/res/data_class.dart';
+import 'package:techno_saz/res/paths.dart';
+
+class SinglePageOfArticles extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var sizeW = MediaQuery.of(context).size.width;
+    var sizeH = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: sizeH / 2.24,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage(Address.img_fammily_single_page),
+                  fit: BoxFit.cover),
+            ),
+            child: SafeArea(
+              child: Stack(
+                children: [
+                  Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(50)),
+                              color: SolidColors.primaryVariantColor),
+                          child: SvgPicture.asset(
+                            Address.save,
+                            width: 25,
+                            height: 25,
+                            colorFilter: ColorFilter.mode(
+                                AdaptiveTheme.of(context).brightness ==
+                                        Brightness.dark
+                                    ? SolidColors.primaryVariantColor
+                                    : SolidColors.iconColor,
+                                BlendMode.srcIn),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(50)),
+                              color: SolidColors.primaryVariantColor),
+                          child: SvgPicture.asset(
+                            Address.arrowLeft,
+                            width: 25,
+                            height: 25,
+                            colorFilter: ColorFilter.mode(
+                                AdaptiveTheme.of(context).brightness ==
+                                        Brightness.dark
+                                    ? SolidColors.primaryVariantColor
+                                    : SolidColors.iconColor,
+                                BlendMode.srcIn),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
