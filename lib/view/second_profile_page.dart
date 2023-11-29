a7b0110c0ff77a749b785e064a47ad29025f7b82
Donya <donya.asayesh@outlook.com>
2023-11-29 12:17:37 +0330

    second profile page

--- lib/view/second_profile_page.dart
+++ lib/view/second_profile_page.dart
@@ -14,7 +14,7 @@ class SecondProfilePage extends StatefulWidget {
 }
 
 class _SecondProfilePageState extends State<SecondProfilePage> {
-   TextEditingController nickName = TextEditingController();
+  TextEditingController nickName = TextEditingController();
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -31,11 +31,8 @@ class _SecondProfilePageState extends State<SecondProfilePage> {
               child: Container(
                 height: 140,
                 width: 140,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(1000),
-                  image: DecorationImage(
-                      image: AssetImage(Address.svg_empety_profile),
-                      fit: BoxFit.cover),
+                child: SvgPicture.asset(
+                  Address.svg_empety_profile,
                 ),
               ),
             ),
@@ -63,37 +60,52 @@ class _SecondProfilePageState extends State<SecondProfilePage> {
                 ],
               ),
             ),
+            
             Padding(
-              padding: EdgeInsets.only(top: sizeH / 62.13),
-              child: Container(
-                width: sizeW / 1.67,
-                decoration: BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(
-                      color: AdaptiveTheme.of(context).brightness ==
-                              Brightness.light
-                          ? SolidColors.textColor
-                          : SolidColors.white,
-                      width: 1.0,
+              padding: EdgeInsets.fromLTRB(size.width / 11.02,
+                  size.height / 34.51, size.width / 11.02, 0),
+              child: TextField(
+                controller: nickName,
+                textDirection: TextDirection.rtl,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(
+                      vertical: size.height / 40, horizontal: 10),
+                  hintText: "دنیا آهنگری",
+                  suffixIconConstraints: BoxConstraints(
+                    maxHeight: 48.0,
+                  ),
+                  suffixIcon: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: SvgPicture.asset(
+                      Address.ic_nickName_eadite,
+                      colorFilter: ColorFilter.mode(
+                          AdaptiveTheme.of(context).brightness ==
+                                  Brightness.dark
+                              ? SolidColors.primaryVariantColor
+                              : SolidColors.iconColor,
+                          BlendMode.srcIn),
                     ),
                   ),
                 ),
               ),
             ),
-            TextField(
+            Padding(
+              padding: EdgeInsets.fromLTRB(
+                  size.width / 11.02, 0, size.width / 11.02, 0),
+              child: TextField(
                 controller: nickName,
                 textDirection: TextDirection.rtl,
                 decoration: InputDecoration(
                   contentPadding: EdgeInsets.symmetric(
                       vertical: size.height / 40, horizontal: 10),
-                  hintText: Strings.nickname_str,
+                  hintText: "donyaAhangari@gmail.com",
                   suffixIconConstraints: BoxConstraints(
                     maxHeight: 48.0,
                   ),
                   suffixIcon: Padding(
                     padding: const EdgeInsets.all(10.0),
                     child: SvgPicture.asset(
-                      Address.ic_nickName_eadite,
+                      Address.ic_email_shape,
                       colorFilter: ColorFilter.mode(
                           AdaptiveTheme.of(context).brightness ==
                                   Brightness.dark
@@ -104,30 +116,7 @@ class _SecondProfilePageState extends State<SecondProfilePage> {
                   ),
                 ),
               ),
-            TextField(
-                controller: nickName,
-                textDirection: TextDirection.rtl,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.symmetric(
-                      vertical: size.height / 40, horizontal: 10),
-                  hintText: Strings.nickname_str,
-                  suffixIconConstraints: BoxConstraints(
-                    maxHeight: 48.0,
-                  ),
-                  suffixIcon: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: SvgPicture.asset(
-                      Address.ic_nickName_eadite,
-                      colorFilter: ColorFilter.mode(
-                          AdaptiveTheme.of(context).brightness ==
-                                  Brightness.dark
-                              ? SolidColors.primaryVariantColor
-                              : SolidColors.iconColor,
-                          BlendMode.srcIn),
-                    ),
-                  ),
-                ),
-              ),
+            ),
             Padding(
               padding: EdgeInsets.only(top: sizeH / 58.25),
               child: SvgPicture.asset(
@@ -140,11 +129,11 @@ class _SecondProfilePageState extends State<SecondProfilePage> {
               child: InkWell(
                 onTap: () {
                   Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => HomePage(),
-                            ),
-                          );
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => HomePage(),
+                    ),
+                  );
                 },
                 child: Container(
                   width: sizeW / 3.071428571428571,
